02eb496ecd35ac2963578ecadf14a0afdc7cb5ff
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-02-25 17:53:50 +0600

    Update debug argument for flask app

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def calculate():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
